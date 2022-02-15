eef44aca3dcbe3fb3f635835de22dafe5f3abe9a
Matteo Milani <mmilani@athics.eu>
2022-02-15 10:58:33 +0100

    aggiornato auth

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,4 +1,12 @@
 import { EntityRepository, Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { User } from './user.entity';
 
 @EntityRepository(User)
-export class UserRepository extends Repository<User> {}
+export class UserRepository extends Repository<User> {
+  async createUser(authcredentialsdto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authcredentialsdto;
+    const user = this.create({ username, password });
+    await this.save(user);
+  }
+}
