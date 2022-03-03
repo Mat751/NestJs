b0038538379146d06a7ddd97d4efd838220168fe
Matteo Milani <mmilani@athics.eu>
2022-03-03 14:46:25 +0100

    aggiornato

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,6 @@
 import { Module } from '@nestjs/common';
-import { GraphQLModule } from '@nestjs/graphql';
-import { LessonModule } from './lesson/lesson.module';
+
 @Module({
-  imports: [
-    GraphQLModule.forRoot({
-      autoSchemaFile: true,
-    }),
-    LessonModule,
-  ],
-  controllers: [],
-  providers: [],
+  imports: [],
 })
 export class AppModule {}
