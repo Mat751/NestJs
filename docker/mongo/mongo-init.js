653a28d2c8ffb41910069e9304d230d6966b90ea
Matteo Milani <mmilani@athics.eu>
2022-03-03 18:27:25 +0100

    mongodb and graphql with getLesson find by id with docker and dockercompose

---
+++ docker/mongo/mongo-init.js
@@ -0,0 +1,11 @@
+db = db.getSiblingDB(_getEnv('MONGO_INITDB_DATABASE'));
+db.createUser({
+  user: _getEnv('MONGO_INITDB_ROOT_USERNAME'),
+  pwd: _getEnv('MONGO_INITDB_ROOT_PASSWORD'),
+  roles: [
+    {
+      role: 'dbOwner',
+      db: _getEnv('MONGO_INITDB_DATABASE'),
+    },
+  ],
+});
